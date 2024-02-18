a42c7700308fcedad3aab2f03f56c482c55692e9
storm_1614 <storm1614@outlook.com>
2024-02-18 17:09:25 +0800

    source

---
+++ README.md
@@ -0,0 +1,13 @@
+### Himawari
+
+![](img/image.png)
+
+这个仓库是用来处理日本气象局管理下的 Himawari 8、9 气象卫星的 L1 数据。  
+ 
+src 下的 `main.py` 是程序，用法如下:  
+```
+./main.py {file.nc}
+```
+
+之后就会处理然后建立一个 `1.png`   
+此 python 程序为个人练手项目，欢迎提出改进建议！  
---
+++ src/main.py
@@ -0,0 +1,73 @@
+#! /bin/python
+
+import os
+import sys
+import netCDF4 as nc
+import numpy as np
+
+from PIL import Image, ImageEnhance, ImageOps
+
+if len(sys.argv) > 1:
+    data = sys.argv[1]
+else:
+    data = "./NC_H09_20240131_2240_R21_FLDK.06001_06001.nc"
+
+
+nc = nc.Dataset(data)
+
+def vis(nc):
+    blue = nc.variables['albedo_01'][:]
+    green = nc.variables['albedo_02'][:]
+    red = nc.variables['albedo_03'][:]
+
+    for i in [blue, green, red]:
+        i = i / np.max(i)
+
+    rgb = np.dstack((red, green, blue))
+    rgb = (rgb * 255).astype(np.uint8)
+
+    imgVis = Image.fromarray(rgb)
+    print("可见光处理完成")
+    return imgVis
+
+def ir(nc):
+    wave = nc.variables['tbb_14'][:]
+    wave = ((wave - np.min(wave)) * (255 - 0)) / (np.max(wave) - np.min(wave)) + 255
+
+    wave = wave.astype(np.uint8)
+
+    imgIR = Image.fromarray(wave)
+    imgIR = ImageOps.invert(imgIR)
+
+    print("红外处理完成")
+    return imgIR
+
+def blend(vis, ir):
+    if ir.size != vis.size:
+        ir = ir.resize(vis.size)
+
+    if ir.mode != vis.mode:
+        ir = ir.convert(vis.mode)
+
+    img = Image.blend(ir, vis, alpha=0.8)
+    
+    print("合并完成")
+    return img
+
+def color(img):
+    #亮度
+    img_1 = ImageEnhance.Brightness(img)
+    img_1 = img_1.enhance(1.3)
+
+    #对比度
+    img_2 = ImageEnhance.Contrast(img_1)
+    img_2 = img_2.enhance(1.1)
+    
+    #饱和度
+    img_3 = ImageEnhance.Color(img_2)
+    img = img_3.enhance(2)
+    print("色彩处理完成")
+    return img
+
+color(blend(vis(nc), ir(nc))).save('./1.png')
+
